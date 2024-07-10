4bb409ca98ac47adc288ef2e550b5f98181dea6c
David Duarte (Williams) <davidduart04@gmail.com>
2024-07-10 10:40:48 -0300

    testeDePush

---
+++ Contador/src/main/java/com/mycompany/contador/Contador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.contador;
+
+/**
+ *
+ * @author david
+ */
+public class Contador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
